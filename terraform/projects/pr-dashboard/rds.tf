23a609d12aac2994541d03cc8de4b50ae6869516
Matt Lee <mattl@stabler.local>
2017-11-07 10:20:48 -0500

    Updated rds.tf::

--- terraform/projects/pr-dashboard/rds.tf
+++ terraform/projects/pr-dashboard/rds.tf
@@ -14,75 +14,76 @@ resource "aws_db_instance" "rds" {
   backup_window = "05:00-18:30"
   maintenance_window = "sun:03:00-sun:04:00"
   allow_major_version_upgrade = true
-  vpc_security_group_ids = [
-    "${aws_security_group.web-platform-tests-db1.id}",
-  ]
-}
 
-
-resource "aws_security_group" "web-platform-tests-db1" {  
-  name = "web-platform-tests-db1"
-
-  description = "RDS postgres servers (terraform-managed)"
-  vpc_id = "${module.vpc.id}"
-
-  # Only postgres in
-  ingress {
-    from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
-    cidr_blocks = [
-      "${var.subnet_cidr_blocks}",
-    ]
-
-  }
-
-  # Allow all outbound traffic.
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# resource "aws_iam_policy" "web-platform-tests-db-backup" {
-#   name = "web-platform-tests-db-backup"
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "s3:ListAllMyBuckets",
-#       "Resource": "arn:aws:s3:::*"
-#     },
-#     {
-#       "Effect": "Allow",
-#       "Action": "s3:*",
-#       "Resource": [
-#         "arn:aws:s3:::${var.nas_backup_bucket}/db",
-#         "arn:aws:s3:::${var.nas_backup_bucket}/db*"
-#       ]
-#     }
+#   vpc_security_group_ids = [
+#     "${aws_security_group.web-platform-tests-db1.id}",
 #   ]
-# }
-# EOF
+}
+
+
+# resource "aws_security_group" "web-platform-tests-db1" {  
+#   name = "web-platform-tests-db1"
+
+#   description = "RDS postgres servers (terraform-managed)"
+#   vpc_id = "${module.vpc.id}"
+
+#   # Only postgres in
+#   ingress {
+#     from_port = 5432
+#     to_port = 5432
+#     protocol = "tcp"
+#     cidr_blocks = [
+#       "${var.subnet_cidr_blocks}",
+#     ]
+
+#   }
+
+#   # Allow all outbound traffic.
+#   egress {
+#     from_port = 0
+#     to_port = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 # }
 
-# resource "aws_iam_user" "web-platform-tests-db-backup" {
-#   name = "web-platform-tests-db-backup"
-# }
+# # resource "aws_iam_policy" "web-platform-tests-db-backup" {
+# #   name = "web-platform-tests-db-backup"
+# #   policy = <<EOF
+# # {
+# #   "Version": "2012-10-17",
+# #   "Statement": [
+# #     {
+# #       "Effect": "Allow",
+# #       "Action": "s3:ListAllMyBuckets",
+# #       "Resource": "arn:aws:s3:::*"
+# #     },
+# #     {
+# #       "Effect": "Allow",
+# #       "Action": "s3:*",
+# #       "Resource": [
+# #         "arn:aws:s3:::${var.nas_backup_bucket}/db",
+# #         "arn:aws:s3:::${var.nas_backup_bucket}/db*"
+# #       ]
+# #     }
+# #   ]
+# # }
+# # EOF
+# # }
 
-# resource "aws_iam_policy_attachment" "web-platform-tests-db-backup" {
-#   name = "web-platform-tests-db-backup"
-#   policy_arn = "${aws_iam_policy.web-platform-tests-db-backup.arn}"
-#   users = ["${aws_iam_user.web-platform-tests-db-backup.id}"]
-# }
+# # resource "aws_iam_user" "web-platform-tests-db-backup" {
+# #   name = "web-platform-tests-db-backup"
+# # }
 
-# # this key was created manually to prevent the secret
-# # from being stored in the tfstate file.
-# # it is available under /mnt/secrets/db-backup-keys
-# #resource "aws_iam_access_key" "db-backup" {
-# #  user = "${aws_iam_user.db-backup.id}"
-# #}
+# # resource "aws_iam_policy_attachment" "web-platform-tests-db-backup" {
+# #   name = "web-platform-tests-db-backup"
+# #   policy_arn = "${aws_iam_policy.web-platform-tests-db-backup.arn}"
+# #   users = ["${aws_iam_user.web-platform-tests-db-backup.id}"]
+# # }
+
+# # # this key was created manually to prevent the secret
+# # # from being stored in the tfstate file.
+# # # it is available under /mnt/secrets/db-backup-keys
+# # #resource "aws_iam_access_key" "db-backup" {
+# # #  user = "${aws_iam_user.db-backup.id}"
+# # #}
