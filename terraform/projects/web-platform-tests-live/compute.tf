0da77779acf0cb6f0568c00a3f4746dfa4a94f49
bmac <bmac325@gmail.com>
2018-07-19 15:02:19 -0400

    Add route53 configs for web-platform-tests.live

--- terraform/projects/web-platform-tests-live/compute.tf
+++ terraform/projects/web-platform-tests-live/compute.tf
@@ -33,3 +33,59 @@ resource "aws_security_group" "web_platform_test_live" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+resource "aws_route53_zone" "web_platform_tests_live" {
+  name = "web-platform-tests.live"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_route53_record" "web_platform_tests_live_A_web_platform_tests_live" {
+  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
+  type = "A"
+  name = "web-platform-tests.live"
+  ttl = "1"
+  records = ["${module.web_platform_tests_live.public_ip}"]
+}
+
+resource "aws_route53_record" "web_platform_tests_live_CNAME_www-web_platform_tests_live" {
+  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
+  type = "CNAME"
+  name = "www"
+  ttl = "1"
+  records = ["web-platform-tests.live"]
+}
+
+resource "aws_route53_record" "web_platform_tests_live_CNAME_xn--lve-6lad-web_platform_tests_live" {
+  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
+  type = "CNAME"
+  name = "xn--lve-6lad"
+  ttl = "1"
+  records = ["web-platform-tests.live"]
+}
+
+resource "aws_route53_record" "web_platform_tests_live_CNAME_www1-web_platform_tests_live" {
+  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
+  type = "CNAME"
+  name = "www1"
+  ttl = "1"
+  records = ["web-platform-tests.live"]
+}
+
+resource "aws_route53_record" "web_platform_tests_live_CNAME_www2-web_platform_tests_live" {
+  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
+  type = "CNAME"
+  name = "www2"
+  ttl = "1"
+  records = ["web-platform-tests.live"]
+}
+
+resource "aws_route53_record" "web_platform_tests_live_CNAME_xn--n8j6ds53lwwkrqhv28a-web_platform_tests_live" {
+  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
+  type = "CNAME"
+  name = "xn--n8j6ds53lwwkrqhv28a"
+  ttl = "1"
+  records = ["web-platform-tests.live"]
+}
