277d1989bbcc182c22ffb1bee539a292c78108df
Mike Pennisi <mike@mikepennisi.com>
2018-07-23 13:06:04 -0400

    Update instance types to reflect reality
    The "size" of the two EC2 instances modified in this commit have been
    manually modified outside of Terraform. Update the configuration so that
    the state of this repository aligns with the state of the deployed
    instances.

--- terraform/projects/pr-dashboard/compute.tf
+++ terraform/projects/pr-dashboard/compute.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "production" {
   ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
   key_name = "${var.key_name}"
   subnet_id = "${element(module.subnet.ids, 0)}"
   vpc_security_group_ids = [
@@ -20,7 +20,7 @@ resource "aws_eip" "production" {
 
 resource "aws_instance" "staging" {
   ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
   key_name = "${var.key_name}"
   subnet_id = "${element(module.subnet.ids, 0)}"
   vpc_security_group_ids = [
