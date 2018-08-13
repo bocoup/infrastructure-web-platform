111587c777e4e0569241a3fab22b5a5a3e83ab1a
Mike Pennisi <mike@mikepennisi.com>
2018-08-13 17:25:12 -0400

    Accommodate dynamism in subdomain requirements
    Because any commit to WPT may change the set of subdomains which must be
    supported, the infrastructure should be provisioned to accommodate any
    subdomain. The deployed machine will be responsible for acknowledging
    the correct host names as they change between revisions of WPT.
    
    > Q. Does Amazon Route 53 support wildcard entries? If so, what record
    > types support them?
    >
    > Yes. To make it even easier for you to configure DNS settings for your
    > domain, Amazon Route 53 supports wildcard entries for all record
    > types, except NS records. A wildcard entry is a record in a DNS zone
    > that will match requests for any domain name based on the
    > configuration you set. For example, a wildcard DNS record such as
    > `*.example.com` will match queries for `www.example.com` and
    > `subdomain.example.com`.
    
    https://aws.amazon.com/route53/faqs/

--- terraform/projects/web-platform-tests-live/network.tf
+++ terraform/projects/web-platform-tests-live/network.tf
@@ -28,46 +28,16 @@ resource "aws_route53_record" "web_platform_tests_live_A_web_platform_tests_live
   records = ["${aws_eip.web_platform_tests_live.public_ip}"]
 }
 
-resource "aws_route53_record" "web_platform_tests_live_CNAME_www-web_platform_tests_live" {
+# WPT may update the set of required subdomains at any time. A "wildcard" CNAME
+# record allows the deployment to accommodate such dynamism without further
+# modification to the DNS configuration.
+resource "aws_route53_record" "web_platform_tests_live_CNAME_wildcard-web_platform_tests_live" {
   zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
   type = "CNAME"
-  name = "www"
+  name = "*"
   ttl = "1"
   records = ["web-platform-tests.live"]
 }
-
-resource "aws_route53_record" "web_platform_tests_live_CNAME_xn--lve-6lad-web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "xn--lve-6lad"
-  ttl = "1"
-  records = ["web-platform-tests.live"]
-}
-
-resource "aws_route53_record" "web_platform_tests_live_CNAME_www1-web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "www1"
-  ttl = "1"
-  records = ["web-platform-tests.live"]
-}
-
-resource "aws_route53_record" "web_platform_tests_live_CNAME_www2-web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "www2"
-  ttl = "1"
-  records = ["web-platform-tests.live"]
-}
-
-resource "aws_route53_record" "web_platform_tests_live_CNAME_xn--n8j6ds53lwwkrqhv28a-web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "xn--n8j6ds53lwwkrqhv28a"
-  ttl = "1"
-  records = ["web-platform-tests.live"]
-}
-
 
 ## alternate domain dns records
 resource "aws_route53_zone" "not_web_platform_tests_live" {
@@ -85,42 +55,13 @@ resource "aws_route53_record" "not_web_platform_tests_live_A_not_web_platform_te
   records = ["${aws_eip.web_platform_tests_live.public_ip}"]
 }
 
-resource "aws_route53_record" "not_web_platform_tests_live_CNAME_www-not_web_platform_tests_live" {
+# WPT may update the set of required subdomains at any time. A "wildcard" CNAME
+# record allows the deployment to accommodate such dynamism without further
+# modification to the DNS configuration.
+resource "aws_route53_record" "not_web_platform_tests_live_CNAME_wildcard-not_web_platform_tests_live" {
   zone_id = "${aws_route53_zone.not_web_platform_tests_live.zone_id}"
   type = "CNAME"
-  name = "www"
-  ttl = "1"
-  records = ["not-web-platform-tests.live"]
-}
-
-resource "aws_route53_record" "not_web_platform_tests_live_CNAME_xn--lve-6lad-not_web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.not_web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "xn--lve-6lad"
-  ttl = "1"
-  records = ["not-web-platform-tests.live"]
-}
-
-resource "aws_route53_record" "not_web_platform_tests_live_CNAME_www1-not_web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.not_web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "www1"
-  ttl = "1"
-  records = ["not-web-platform-tests.live"]
-}
-
-resource "aws_route53_record" "not_web_platform_tests_live_CNAME_www2-not_web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.not_web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "www2"
-  ttl = "1"
-  records = ["not-web-platform-tests.live"]
-}
-
-resource "aws_route53_record" "not_web_platform_tests_live_CNAME_xn--n8j6ds53lwwkrqhv28a-not_web_platform_tests_live" {
-  zone_id = "${aws_route53_zone.not_web_platform_tests_live.zone_id}"
-  type = "CNAME"
-  name = "xn--n8j6ds53lwwkrqhv28a"
+  name = "*"
   ttl = "1"
   records = ["not-web-platform-tests.live"]
 }
